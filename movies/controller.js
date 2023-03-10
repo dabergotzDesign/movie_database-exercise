e483d03ffa8ab854f628ccbf32b4e868c006fa2d
dabergotzDesign <otakulp@gmx.de>
2023-03-10 12:23:53 +0100

    controller

---
+++ movies/controller.js
@@ -0,0 +1,10 @@
+const data = [
+    {id: 1, title: "Everything, Everywhere, All At Once", year: "2022"},
+    {id: 2, title: "Babylon", year: "2022"},
+    {id: 3, title: "Godzilla - King of the Monsters", year: "2019"},
+];
+
+export function listAction(req, res){
+    res.send(data);
+}
+
--- movies/index.js
+++ movies/index.js
@@ -1,17 +1,11 @@
 "use strict";
 
 import { Router } from "express";
+import {listAction} from "./controller.js";
 
 const router = Router();
 
-const data = [
-    {id: 1, title: "Everything, Everywhere, All At Once", year: "2022"},
-    {id: 2, title: "Babylon", year: "2022"},
-    {id: 3, title: "Godzilla - King of the Monsters", year: "2019"},
-];
 
-router.get("/", (req, res)=>{
-    res.send(data)
-});
+router.get("/", listAction);
 
 export {router};
