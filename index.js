00bc7c10f8bfe3744926717a7cdb573b5d667e27
dabergotzDesign <otakulp@gmx.de>
2023-03-13 15:09:46 +0100

    ejs templates

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ import { router as movieRouter } from "./movies/index.js";
 
 const app = express();
 
+app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: false}));
 
 app.use("/movies", movieRouter);
 
-app.get("/", (req, res)=> res.redirect("/movies"));
+//app.get("/", (req, res)=> res.redirect("/movies"));
+
+app.get("/", (req, res)=> {
+    res.render("index");
+});
 
 app.listen(8081, () =>{
     console.log("Server is listening to http://localhost:8081")
